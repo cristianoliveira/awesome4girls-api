664e6893f075c69c28fbdea2b7203b0137fc9098
Cristian Oliveira <contato@cristianoliveira.com.br>
2016-12-04 21:59:22 -0200

    fix: sync return

--- app/controllers/sync.rb
+++ app/controllers/sync.rb
@@ -4,6 +4,6 @@ class SyncController < ApiController
   post '/' do
     restricted_to!(User::ROLE_ADMIN) { |name| User.find_by_name(name) }
 
-    jsonapi({worker: SincronizerWorker.perform_async}, is_collection: false)
+    json(worker: SincronizerWorker.perform_async)
   end
 end
